2d090e5679022f9dee8028bc34a959b54a4287e1
neilblaze <putubanerjee23@gmail.com>
2023-12-17 21:26:50 +0530

    define audio and image processing function

--- model/app.py
+++ model/app.py
@@ -19,3 +19,12 @@ def pad_image(image):
         new_image = Image.new(image.mode, (h, h), (0, 0, 0))
         new_image.paste(image, ((h - w) // 2, 0))
         return new_image
+    
+def calculate(image_in, audio_in):
+    waveform, sample_rate = torchaudio.load(audio_in)
+    waveform = torch.mean(waveform, dim=0, keepdim=True)
+    torchaudio.save("/home/demo/source/audio.wav", waveform, sample_rate, encoding="PCM_S", bits_per_sample=16)
+    image = Image.open(image_in)
+    image = pad_image(image)
+    image.save("/home/demo/source/image.png")
+

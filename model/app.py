8cc18b6951ed02610a4272fc001817d40419052c
neilblaze <putubanerjee23@gmail.com>
2023-12-17 21:26:21 +0530

    define image padding function

--- model/app.py
+++ model/app.py
@@ -6,3 +6,16 @@ from PIL import Image
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+def pad_image(image):
+    w, h = image.size
+    if w == h:
+        return image
+    elif w > h:
+        new_image = Image.new(image.mode, (w, w), (0, 0, 0))
+        new_image.paste(image, (0, (w - h) // 2))
+        return new_image
+    else:
+        new_image = Image.new(image.mode, (h, h), (0, 0, 0))
+        new_image.paste(image, ((h - w) // 2, 0))
+        return new_image
